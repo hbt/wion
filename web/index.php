95aad2e7e4d2f12031e1bbdd4dcfe011f5f0b586
hbt <hassenbentanfous@gmail.com>
2018-08-11 06:52:39 -0400

    wip

--- web/index.php
+++ web/index.php
@@ -2,17 +2,16 @@
 
 /**
  * web UI
- * 
+ *
  * php -S 0.0.0.0:8989 index.php
- * 
- * 
+ *
+ *
  */
 
 include(__DIR__ . DIRECTORY_SEPARATOR . 'lib.php');
 
 function main()
 {
-
 }
 
 main();
@@ -21,14 +20,13 @@ var_dump(getState());
 if(array_key_exists('cmd', $_GET))
 {
     $cmd = ($_GET['cmd']);
-    
-    $cmd = str_ireplace('%s20', ' ', $cmd);
+
+    $cmd  = str_ireplace('%s20', ' ', $cmd);
     $args = explode(' ', $cmd);
-    
-    
-    $argv = array_merge(['main.php'],  explode(' ', $cmd));
+
+
+    $argv = array_merge(['main.php'], explode(' ', $cmd));
     timerCLI($argv);
-    
 }
 
 ?>
@@ -36,31 +34,37 @@ if(array_key_exists('cmd', $_GET))
 <body style="background-color: black; color: white">
 
 <h1>
-    
-<br/>
-<br/>
 
-<a href="./index.php?cmd=start">START Timer</a>
-<br/>
-<br/>
+    <br/>
+    <br/>
 
-<a href="./index.php?cmd=stop">STOP Timer</a>
-<br/>
-<br/>
+    <a href="./index.php?cmd=start">START Timer</a>
+    <br/>
+    <br/>
 
-<a href="./index.php?cmd=set 10 10">Set 10 10</a>
-<br/>
-<br/>
+    <a href="./index.php?cmd=stop">STOP Timer</a>
+    <br/>
+    <br/>
 
-<a href="./index.php?cmd=set 10 20">Set 10 20</a>
-<br/>
-<br/>
+    <a href="./index.php?cmd=set 10 10">Set 10 10</a>
+    <br/>
+    <br/>
+
+    <a href="./index.php?cmd=set 20 10">Set 20 10</a>
+    <br/>
+    <br/>
+
+    <a href="./index.php?cmd=set 10 20">Set 10 20</a>
+    <br/>
+    <br/>
 
 
-<a href="./index.php?cmd=set 20 10">Set 20 10</a>
-<br/>
-<br/>
-    
+    <a href="./index.php?cmd=set 10 30">Set 10 30</a>
+    <br/>
+    <br/>
+
+
+
 </h1>
 
 </body>
